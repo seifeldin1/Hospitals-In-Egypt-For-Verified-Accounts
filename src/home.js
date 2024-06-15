b8d4d32f2fdf6364e207b3cf2617cf3ac4cc1113
seifeldin1 <seif1442004@gmail.com>
2024-06-15 12:55:50 +0300

    updated home view

--- src/home.js
+++ src/home.js
@@ -18,7 +18,7 @@ const HomePage=()=>{
             <h1>Home Page</h1>
             <br></br>
             {isAuthenticated ? <h1><Link to ="/hospitals">Click here</Link> to go to hospitals list</h1>: <p>User is not authenticated</p>}
-            <h1><Link to="/login">Login</Link>to our website</h1>
+            <h1><Link to="/login">Login</Link> to our website</h1>
         </div>
     )
 }
